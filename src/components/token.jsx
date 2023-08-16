e2cd0be01357fb774e1978e4b8cf5c53446c6d7b
Osariemen Aibueku <102379269+Osariemen7@users.noreply.github.com>
2023-08-16 21:15:42 +0100

    Add files via upload

--- src/components/token.jsx
+++ src/components/token.jsx
@@ -1,29 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Select from 'react-select';
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import PasswordChecklist from "react-password-checklist"
 
-function YourComponent() {
-  const [selectedOption, setSelectedOption] = useState(null)
-  const [info, setInfo] = useState([])
-  const [loading, setLoading] = useState(true)
-  const navigate = useNavigate()
-  let tok= JSON.parse(localStorage.getItem("user-info"));
-    const terms = (tok) => {
-       let refreshval;
+function TokenPage() {
+    const [password1, setPassword1] = useState("");
+    const [password2, setPassword2] = useState("");
+    const [message, setMessage] = useState('')
+    const [info, setInfo] = useState('')
+    const [otp, setOtp] = useState('');
+    const [passwordType, setPasswordType] = useState("password");
+    const navigate = useNavigate()
 
-  if ( tok === null || typeof tok === "undefined" ) {
-    refreshval = 0;
-  } else {
-    refreshval = tok.refresh_token;
+    const nav=()=>{
+      navigate('/components/login')
+    }
+   const otpChange =(event) =>{
+      setOtp(event.target.value)
+   }
+
+   const togglePassword =()=>{
+    if(passwordType==="password")
+    {
+     setPasswordType("text")
+     return;
+    }
+    setPasswordType("password")
   }
+  
+const handlePasswordChange =(evnt)=>{
+        
+  setPassword1(evnt.target.value);
+}
+const handlePasswordConfirm =(evnt)=>{
+        
+  setPassword2(evnt.target.value);
+}
 
-  return refreshval;
-};
-let refresh = terms(tok)
-
-const fetchDa = async () => {
-    let item ={refresh}
-    let rep = await fetch ('https://sandbox.prestigedelta.com/refreshtoken/',{
+async function passChange(e) {
+  e.preventDefault();
+  let new_password = password1
+    console.warn( password1, password2)
+    let item = {new_password, otp};
+    let resut = await fetch ('https://sandbox.prestigedelta.com/resetpassword/',{
         method: 'POST',
         headers:{
           'Content-Type': 'application/json',
@@ -32,52 +50,50 @@ const fetchDa = async () => {
      body:JSON.stringify(item)
     });
     
-    rep = await rep.json();
-    let bab = rep.access_token
-  let response = await fetch("https://sandbox.prestigedelta.com/getbanklist/",{
-  method: "GET",
-  headers:{'Authorization': `Bearer ${bab}`},
-  })
-  //localStorage.setItem('user-info', JSON.stringify(tok))
-
-  if (response.status === 401) {
-    navigate('/components/login');
-  } else {  
-  response = await response.json();
-  setLoading(false)
-  setInfo(response)
-    }}
-    useEffect(() => {
-      fetchDa()
-    }, [])
-
-    const handleBank = (selectedOption) => {
-      setSelectedOption(selectedOption);
-    };
-  
-    const options = info.map((item) => ({
-      label: item.bank_name,
-      value: item.bank_code,
-    }));
-    console.log(selectedOption.value)
-if(loading){
-  return(
-    <div>Loading...</div>
-  )
-}
+    if (resut.status !== 200) {
+      setMessage('Invalid Information');
+    } else {
+      resut = await resut.json();
+      setInfo(JSON.stringify(resut))
+    
+    }
+  }
+   if (info=== '')
   return (
-    <div>
-      <Select
-      onChange={handleBank}
-      className="line"
-      placeholder="Select Bank"
-      options={options}
-      isSearchable={true}
-      value={selectedOption}
-    />
   
+
+    <div>
+        <Link to='/components/login'><i class="fa-solid fa-chevron-left bac"></i></Link>
+     <p className="lp">Fill in new Password and OTP</p>
+
+<form>
+    <p className='sp'>OTP</p>
+    <input className="lin"  onChange={otpChange} type="number" name="username"  required/><br/><br/>
+    <p className="sp">Password</p>
+    <input className="line" type={passwordType} onChange={handlePasswordChange}  name="password" required/>
+    { passwordType==="password"?
+             <i onClick={togglePassword} class="fa-regular fa-eye-slash ic"></i> : <i class="fa-regular fa-eye ic" onClick={togglePassword}></i>} <br/>
+            <p className='sp'>Confirm Password</p>
+            <input className="line" type={passwordType} onChange={handlePasswordConfirm} name='password2' />
+            { passwordType==="password"?
+             <i onClick={togglePassword} class="fa-regular fa-eye-slash ic"></i> : <i class="fa-regular fa-eye ic" onClick={togglePassword}></i>} <br/>
+    <button className="logb" onClick={passChange} type="submit">Log in</button>
+    <div className="message">{message ? <p>{message}</p> : null}</div>
+    <PasswordChecklist
+				rules={["minLength","specialChar","number","capital","match"]}
+				minLength={8}
+				value={password1}
+				valueAgain={password2}
+			/>
+</form> 
     </div>
   );
+  return(
+     <div className='plus'>
+           <p>Password reset successful!</p>
+           <button className='logb' onClick={nav}>Login</button>
+     </div>
+  )
 }
 
-export default YourComponent;
+export default TokenPage;
--- src/components/transact.jsx
+++ src/components/transact.jsx
@@ -14,6 +14,11 @@ const terms = (tok) => {
 };
 let refresh = terms(tok)
 
+const currentDate = new Date(); // Get the current date
+
+    const thirtyDaysBefore = new Date(); // Create a new Date object
+    thirtyDaysBefore.setDate(currentDate.getDate() - 90)  
+
 const Transact = () =>{
     const [info, setInfo] = useState('')
     const [loading, setLoading] = useState(true);
@@ -29,7 +34,7 @@ const Transact = () =>{
       });
       rep = await rep.json();
       let bab = rep.access_token
-    let response = await fetch("https://sandbox.prestigedelta.com/projectlist/",{
+    let response = await fetch(`https://sandbox.prestigedelta.com/transactions/?start_date=01/31/2022&end_date=${(new Date()).toLocaleDateString('en-GB')}`,{
     method: "GET",
     headers:{'Authorization': `Bearer ${bab}`},
     })
@@ -50,19 +55,23 @@ const Transact = () =>{
         <div>
             <Link to='/components/project'><i class="fa-solid fa-chevron-left bac"></i></Link>
             <h4 className="dh3">transaction History</h4>
-            {info[0].transactions.map((obj, index) => 
-                  <div className='td'>
-                  <div className='tl'>
-                       <p key={index}>{obj.amount}</p>
-                       <p key={index}>{(new Date(obj.time)).toLocaleString('en-GB')}</p>
-                  </div>
-                  <div className='tg'>
-                       <p key={index}>{obj.classification}</p>
-                       
-                  </div>
-        
-                  </div>
-                       )}
+            {info.map((obj, index) => (
+  <div key={index}>
+    {obj.transactions.map((transaction, transactionIndex) => (
+      <div key={transactionIndex} className='td'>
+        <div className='tl'>
+          <p>{transaction.classification}</p>
+          <p>{transaction.amount}</p>
+        </div>
+        <div className='tg'>
+          <p>{transaction.status}</p>
+          <p>{new Date(transaction.time).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true })}</p>
+        </div>
+        <p className='tm'>{transaction.description}</p>
+      </div>
+    ))}
+  </div>
+))}
         </div>
     )
 }
